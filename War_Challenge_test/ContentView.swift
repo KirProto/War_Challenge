bd66638a0912cade0ca16c2c7467a99c405a528f
Kir Proto Q <kir.proto@ya.ru>
2022-02-24 09:58:39 +0500

    Initial Commit

---
+++ War_Challenge_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  War_Challenge_test
+//
+//  Created by KIR Q on 2022.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War_Challenge_test/War_Challenge_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Challenge_testApp.swift
+//  War_Challenge_test
+//
+//  Created by KIR Q on 2022.02.24.
+//
+
+import SwiftUI
+
+@main
+struct War_Challenge_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
